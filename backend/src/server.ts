c6d07301d7a6c7d81bc9fc51e92632282e0337e1
Higor Menezes <higormenezes1996@gmail.com>
2020-10-19 00:08:26 -0300

    added cors to api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "express-async-errors";
 import path from "path";
 import errorHandler from "./errors/handler";
@@ -6,6 +7,8 @@ import "./database/connection";
 import routes from "./routes";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
